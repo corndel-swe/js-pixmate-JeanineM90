1cc44dab0e47ef57a0409dfd3512d687e2187aa8
Jeanine Marteau <jeanine.marteau@iris.co.uk>
2024-06-12 11:25:05 +0100

    solves 1a

--- exercises/1a.js
+++ exercises/1a.js
@@ -12,5 +12,22 @@
  * @returns {number} The sum of the even numbers smaller than or equal to n
  */
 export function addEven(n) {
-  // TODO
+  // TODO: 
+  /*
+    Initiate a sum variable
+    Count from 1 to n (e.g 8)
+    Take each even number
+    Add it to a sum
+    Return a sum
+  */
+  let i = 1
+  let sum = 0
+  while (i <= n) {
+    if (i % 2 == 0) {
+      sum += i
+    }
+    i++
+  }
+  return sum
 }
+
