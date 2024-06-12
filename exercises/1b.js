3fc8c91f2a4200388c6ebaec977b9c5d9821bb8b
Jeanine Marteau <jeanine.marteau@iris.co.uk>
2024-06-12 13:52:58 +0100

    1b with example

--- exercises/1b.js
+++ exercises/1b.js
@@ -11,5 +11,34 @@
  * @returns {number} The sum of the array
  */
 export function sumArray(arr) {
-  // TODO
+  // TODO:
+  /*
+  - sum the values in arr (the array of numbers)
+  - return result
+  
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i]
+  }
+    this used sum istead 
+*/
+
+  let result = 0
+
+  for (let i = 0; i < arr.length; i++) {
+    result += arr[i] // one of the numbers in the array e.g arr[2] is the 3rd number in the array - the value of this depends on where you are in the loop (which stage of the loop e.g first round)
+  }
+
+  return result
+
 }
+
+//what ia arr:
+const whatIs = sumArray([1, 3, 7])
+console.log(whatIs)
+
+// loop through array in "for loop" tech doc, then use sum (used in 1a)
+// this doc also has an example for accumulating a value e.g add all the odd numbers less than 100 
+
+
--- exercises/2a.js
+++ exercises/2a.js
@@ -14,3 +14,4 @@
 export function sumSelective(numbers) {
   // TODO
 }
+
